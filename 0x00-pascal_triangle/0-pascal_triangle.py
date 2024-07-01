e0c202f7020c43694082bdefced897612e9802b8
Sanieeme <shoneesani@gmail.com>
2024-07-01 02:53:13 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" returns a list of lists of integers """
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    t = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(t[i-1][j-1] + t[i-1][j])
+        row.append(1)
+        t.append(row)
+    return t

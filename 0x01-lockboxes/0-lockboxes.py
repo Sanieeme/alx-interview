11ee3a58ba19438c7aca91ae432f43a5080f6195
Sanieeme <shoneesani@gmail.com>
2024-07-03 04:42:02 +0000

    comments

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Return True if all boxes can be opened, else return False
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    returns true or false
+
+    Args:
+         boxes: list of lists
+    Return:
+         true or false
+    """

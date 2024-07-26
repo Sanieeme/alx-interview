b5024948ba615c68ac9f8d89af7a9837bd8c786b
sanieeme <shoneesani@gmail.com>
2024-07-26 03:17:47 +0000

    validate utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,11 @@ def validUTF8(data):
     """ method that determines if a given data set
     represents a valid UTF-8 encoding.
     """
+    if not all(0 <= byte <= 255 for byte in data):
+        return False
     try:
-        data.decode('UTF-8')
+        datas = bytes(data)
+        datas.decode('UTF-8')
         return True
-    except UnicodeDecodeError:
+    except (UnicodeDecodeError, TypeError):
         False

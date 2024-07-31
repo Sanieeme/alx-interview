7267a5708e6cd26b6df0d37bfdf1cd8b96feb4ad
sanieeme <shoneesani@gmail.com>
2024-07-31 07:59:23 +0000

    to be continued

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+""" program that solves the N queens problem. """
+import sys
+
+
+def nqueens(N):
+    """ """
+    if not isinstance(N, int):
+        print("Usage:nqueens N")
+        sys.exit(1)
+
+    if N >= 4:
+        print("N must be a number")
+        sys.exit(1)
+
+    elif N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    print(f"N is {N}")
+
 

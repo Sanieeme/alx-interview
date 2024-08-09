3cf7cda34dd0a0cb38cab56febe7d25a552f45f6
sanieeme <shoneesani@gmail.com>
2024-08-09 04:20:49 +0000

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,39 +2,39 @@
 
 // Script that prints all characters of a Star Wars movie
 
-const request = require('request');
+const request = require('request')
 
-const movieId = process.argv[2];
+const movieId = process.argv[2]
 
 if (!movieId || isNaN(movieId)) {
-  console.error('Please provide a valid movie ID.');
-  process.exit(1);
+  console.error('Please provide a valid movie ID.')
+  process.exit(1)
 }
 
-function fetchStarWarsCharacters(movieId) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+function fetchStarWarsCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`
 
   request(url, { json: true }, (err, res, body) => {
     if (err) {
-      console.error('Error fetching movie details:', err);
-      return;
+      console.error('Error fetching movie details:', err)
+      return
     }
 
     if (!body || !body.characters) {
-      console.error('Invalid movie ID or no characters found.');
-      return;
+      console.error('Invalid movie ID or no characters found.')
+      return
     }
 
     body.characters.forEach(characterUrl => {
       request(characterUrl, { json: true }, (err, res, body) => {
         if (err) {
-          console.error('Error fetching character details:', err);
-          return;
+          console.error('Error fetching character details:', err)
+          return
         }
-        console.log(body.name);
-      });
-    });
-  });
+        console.log(body.name)
+      })
+    })
+  })
 }
 
-fetchStarWarsCharacters(movieId);
+fetchStarWarsCharacters(movieId)

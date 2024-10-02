f8a247c4225417b964fbd509264ed65d99a7ad0d
sanieeme <shoneesani@gmail.com>
2024-10-02 06:27:37 +0000

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""function"""
+"""function that change comes from within"""
 
 
 def makeChange(coins, total):
     """determine the fewest number of coins
     needed to meet a given amount total
     """
-    num = [float('inf')] * (total + 1)
-    num[0] = 0
+    nums = [float('inf')] * (total + 1)
+    nums[0] = 0
     if total <= 0:
         return 0
     for coin in coins:
         for i in range(coin, total + 1):
-            num[i] = min(num[i], num[i - coin] + 1)
-    if num[total] != float('inf'):
-        return num[total]
+            nums[i] = min(nums[i], nums[i - coin] + 1)
+    if nums[total] != float('inf'):
+        return nums[total]
     else:
         return -1
